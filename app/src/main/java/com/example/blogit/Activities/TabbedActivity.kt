12433df82311c5781662fbba30b9181ca4520590
manassogale132 <manassogale@gmail.com>
2021-04-03 09:52:23 +0530

    Added : Custom toolbar with logout menuitem.

--- app/src/main/java/com/example/blogit/Activities/TabbedActivity.kt
+++ app/src/main/java/com/example/blogit/Activities/TabbedActivity.kt
@@ -1,15 +1,17 @@
 package com.example.blogit.Activities
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
 import com.example.blogit.Activities.ui.main.SectionsPagerAdapter
 import com.example.blogit.R
+import com.google.android.material.tabs.TabLayout
+
 
 class TabbedActivity : AppCompatActivity() {
 
@@ -21,5 +23,26 @@ class TabbedActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var menuInflater : MenuInflater = getMenuInflater()
+        menuInflater.inflate(R.menu.app_bar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return when (id) {
+            R.id.menuLogout -> {
+                Toast.makeText(applicationContext, "Logout Operation", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
